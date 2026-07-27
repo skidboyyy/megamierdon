8ec8fb907a3fdf03ed69eaee79bdd5b97b04966e
skidboyyy <salvadormatiasjesus2012@gmail.com>
2026-07-26 22:23:55 -0400

    Add GUI with warning message to bloxkid.lua

--- bloxkid.lua
+++ bloxkid.lua
@@ -1,1 +1,14 @@
-print("fuck u")
+local screenGui = Instance.new("ScreenGui")
+screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+local textLabel = Instance.new("TextLabel")
+textLabel.Parent = screenGui
+
+textLabel.Size = UDim2.new(1, 0, 1, 0)
+textLabel.Position = UDim2.new(0, 0, 0, 0)
+textLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+
+textLabel.Text = "never try to scam daddy nexxuzz"
+textLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+textLabel.TextScaled = true
+textLabel.Font = Enum.Font.GothamBold
